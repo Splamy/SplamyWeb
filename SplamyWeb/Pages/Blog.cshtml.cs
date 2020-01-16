21ed2b18cd98810949c607111f831f595188e26c
Lukas Jagemann <splamyn@gmail.com>
2020-01-16 15:05:03 +0100

    Bloggie

---
+++ SplamyWeb/Pages/Blog.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+
+namespace SplamyWeb.Pages
+{
+	public class BlogModel : PageModel
+	{
+		// Features:
+		// Create new (POST/PUT)
+		// Edit (PATCH)
+		// Delete (DELETE)
+		
+		// Optional:
+		// Hide
+
+		// Requirements
+		// Edit Field (+opt preview field)
+
+		public void OnGet()
+		{
+		}
+
+		public IEnumerable<BlogEntry> GetBlogs()
+		{
+			yield break;
+		}
+	}
+
+	public class BlogEntry
+	{
+		public string? Header { get; set; }
+		public string? Summary { get; set; }
+		public string[] Tags { get; set; } = Array.Empty<string>();
+		public string? ContentRaw { get; set; }
+		public string? ContentHtml { get; set; }
+	}
+}
