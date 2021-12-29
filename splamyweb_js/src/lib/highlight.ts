45afb7d0586bf6cbb406647ca5214aa4ee6817fb
Splamy <splamyn@gmail.com>
2021-12-29 19:34:19 +0100

    fix highlight import

--- splamyweb_js/src/lib/highlight.ts
+++ splamyweb_js/src/lib/highlight.ts
@@ -1,8 +1,8 @@
 import Prism from 'prismjs';
 import "prismjs/themes/prism-tomorrow.css";
-import "prismjs/components/prism-csharp";
-import "prismjs/components/prism-cil";
-import "prismjs/plugins/line-numbers/prism-line-numbers";
+import "prismjs/components/prism-csharp.js";
+import "prismjs/components/prism-cil.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.js";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
 export default Prism;
