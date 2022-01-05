70b35c6fe0dd4864b0471fe953257dc4895545be
Splamy <splamyn@gmail.com>
2022-01-05 01:17:30 +0100

    Fixed fallback calculation

--- SplamyWeb/Controllers/Fallback.cs
+++ SplamyWeb/Controllers/Fallback.cs
@@ -19,30 +19,77 @@ namespace SplamyWeb.Controllers
 		public IActionResult Index()
 		{
 			var reqPath = Request.Path.Value;
-			if (reqPath is null || reqPath.Contains('.'))
+			if (reqPath is null || reqPath.Contains(".."))
 				return NotFound();
 
 			const string IndexEndStrip = "index.html";
 			if (reqPath.EndsWith(IndexEndStrip, StringComparison.OrdinalIgnoreCase))
 				reqPath = reqPath[..^IndexEndStrip.Length];
 
-			var fullReqPath = Path.Join(appEnvironment.WebRootPath, reqPath);
 			try
 			{
-				var target = Directory.GetFiles(fullReqPath, "index.html", new EnumerationOptions { MatchCasing = MatchCasing.CaseInsensitive }).FirstOrDefault();
-				if (target is not null)
+				var foundFilePath = TryFindFilePath(reqPath);
+				if (foundFilePath is not null)
 				{
-					var targetPath = target.AsSpan();
-					targetPath = targetPath[..fullReqPath.Length];
+					var targetPath = foundFilePath.AsSpan();
+					targetPath = targetPath[appEnvironment.WebRootPath.Length..];
 					if (targetPath.EndsWith(IndexEndStrip, StringComparison.OrdinalIgnoreCase))
 						targetPath = targetPath[..^IndexEndStrip.Length];
 					targetPath = targetPath.TrimEnd(new char[] { '\\', '/' });
-					return RedirectPermanent(targetPath.ToString());
+					return RedirectPermanent(targetPath.ToString().Replace('\\', '/'));
 				}
 			}
 			catch { }
 
 			return Redirect("/error?req=" + Uri.EscapeDataString(Request.Path.Value ?? ""));
+		}
+
+		public string? TryFindFilePath(string reqPath)
+		{
+			var reqPathSplit = reqPath.TrimStart(new char[] { '\\', '/' }).Split('/');
+			var pathParts = reqPathSplit.Take(..^1);
+			var filePart = reqPathSplit.Last();
+
+			// May come in as
+			// test          => [test.html, test/index.html]
+			// test.html     => [test.html]
+			// test/         => [test/index.html]
+			// test/sub      => [test/sub.html, test/sub/index.html]
+			// test/sub.html => [test/sub.html]
+
+			var currentPath = new DirectoryInfo(appEnvironment.WebRootPath);
+			foreach (var part in pathParts)
+			{
+				var foundFolder = currentPath.EnumerateDirectories().FirstOrDefault(folder =>
+				{
+					return string.Equals(folder.Name, part, StringComparison.OrdinalIgnoreCase);
+				});
+				if (foundFolder is null)
+					return null;
+				currentPath = foundFolder;
+			}
+
+			var foundFile = currentPath.EnumerateFiles()
+				.FirstOrDefault(file => string.Equals(file.Name, filePart, StringComparison.OrdinalIgnoreCase));
+
+			if (foundFile is not null)
+				return foundFile.FullName;
+
+			var indexFolder = currentPath.EnumerateDirectories()
+				.FirstOrDefault(folder => string.Equals(folder.Name, filePart, StringComparison.OrdinalIgnoreCase));
+
+			if (indexFolder is null)
+				return null;
+
+			currentPath = indexFolder;
+			foundFile = currentPath.GetFiles("index.html", new EnumerationOptions { MatchCasing = MatchCasing.CaseInsensitive }).FirstOrDefault();
+
+			if (foundFile is null)
+				return null;
+
+			// TODO check if this is final here
+
+			return foundFile.FullName;
 		}
 	}
 }
