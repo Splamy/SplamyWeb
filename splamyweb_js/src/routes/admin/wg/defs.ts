51eff07379d752382eab74bf59a20e44cf34b0df
Splamy <splamyn@gmail.com>
2023-09-27 22:46:04 +0200

    cleanup

--- splamyweb_js/src/routes/admin/wg/defs.ts
+++ splamyweb_js/src/routes/admin/wg/defs.ts
@@ -11,25 +11,12 @@ export interface EditWgPeer {
 	allowedIPs: string;
 }
 
-
 export function b64ToArray(str: string) {
 	return Uint8Array.from(atob(str), (c) => c.charCodeAt(0));
 }
 
 export function arrayToB64(arr: Uint8Array) {
 	return btoa(String.fromCharCode(...arr));
-}
-
-export function toEditPeerList(peers: WgPeer[]) {
-	return peers.map(toEditPeer);
-}
-
-export function toEditPeer(peer: WgPeer): EditWgPeer {
-	return {
-		publicKey: peer.publicKey,
-		friendlyName: peer.friendlyName,
-		allowedIPs: peer.allowedIPs.join(', ')
-	};
 }
 
 export function fromEditPeer(peer: EditWgPeer): WgPeer {
