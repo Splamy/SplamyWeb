1905de7a0fb6a97058a44691a90f5a49f9ed46e3
Splamy <splamyn@gmail.com>
2025-06-08 00:42:46 +0200

    bun stuff

--- deplopy.nu
+++ deplopy.nu
@@ -1,2 +1,2 @@
 dotnet publish SplamyWeb -c Release --sc -r linux-x64 /p:PublishSingleFile=true -o ./publish
-rclone copy ./publish splamy_web:splamy_web
+rclone copy ./publish splamy_web:splamy_web -P
--- splamyweb_js/publish.ps1
+++ splamyweb_js/publish.ps1
@@ -1,5 +1,5 @@
-yarn install
-yarn build
+bun install
+bun run build
 rm -Force -Recurse ..\SplamyWeb\wwwroot\*
 New-Item -Name ..\SplamyWeb\wwwroot\.gitkeep -ItemType File
 cp -Force -Recurse .\build\* ..\SplamyWeb\wwwroot\
